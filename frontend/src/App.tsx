8b624919c1fab621a910d75e0e55249bbeb6a4ce
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-04-25 00:13:23 -0600

    frontend shows all images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from 'react'
+import {Api, Image as ImageInfo, Metadata} from "./Api.js";
+import {AppShell, Center, Flex, Footer, Header, Image, Pagination, Text} from "@mantine/core";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState<ImageInfo[]>([])
+  const [metadata, setMetadata] = useState<Metadata | null>(null)
+  const [page, setPage] = useState(1)
+
+  const api = new Api()
+  const getURL = (id: string) => `${api.baseUrl}/${encodeURIComponent(id)}`
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const res = await api.getImages({page: page-1})
+      if (res.data.items) {
+        setImages(res.data.items)
+      }
+      setMetadata(res.data.metadata || null)
+    }
+    fetchImages().catch(console.error)
+  }, [page])
+
+  const header = (
+    <Header height={{base: 50, md: 70}} p="md" fixed>
+      <div style={{display: 'flex', alignItems: 'center', height: '100%'}}>
+        <Text>Application header</Text>
+      </div>
+    </Header>
+  )
+  const footer = (
+    <Footer height={{base: 30, md: 50}} fixed>
+      <Center>
+        <Pagination total={metadata?.totalPages || 0} onChange={setPage}/>
+      </Center>
+    </Footer>
+  )
+  const imageList = (
+    images.map((image) => (
+      <Image key={image.id} src={getURL(image.id)} alt={image.prompt} radius="md" width={512} height={768}
+             fit="contain" withPlaceholder/>
+    ))
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AppShell padding="md" header={header} footer={footer}>
+      <Flex wrap="wrap" justify="center" gap={{base: 'sm', sm: 'lg'}}>
+        {imageList}
+      </Flex>
+    </AppShell>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import {MantineProvider} from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={{colorScheme: 'dark'}} withGlobalStyles withNormalizeCSS>
+      <App/>
+    </MantineProvider>
   </React.StrictMode>,
 )
