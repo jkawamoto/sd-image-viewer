48fbd2100c3b4038f151c108b773d651cc2d60d6
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-04-25 22:18:17 -0600

    refactor: querying images

--- frontend/src/Query.tsx
+++ frontend/src/Query.tsx
@@ -1,24 +1,22 @@
-import {useState} from "react";
+import {KeyboardEvent, useCallback} from "react";
 import {TextInput} from "@mantine/core";
+import {useInputState} from "@mantine/hooks";
 
 type Props = {
   onSearch: (query: string) => void
 }
 
 function Query({onSearch}: Props) {
-  const [query, setQuery] = useState("")
+  const [query, setQuery] = useInputState("")
+  const onKeyUp = useCallback((event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault()
+      onSearch(query)
+    }
+  }, [query, onSearch])
 
   return (
-    <TextInput placeholder="Search keywords"
-               value={query}
-               onChange={(event) => setQuery(event.currentTarget.value)}
-               onKeyUp={(event) => {
-                 if (event.key === "Enter") {
-                   event.preventDefault()
-                   onSearch(query)
-                 }
-               }}
-    />
+    <TextInput placeholder="Search keywords" value={query} onChange={setQuery} onKeyUp={onKeyUp}/>
   )
 }
 
