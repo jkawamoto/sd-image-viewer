fdf509139523d80418eebce3ca54bec472ce2ff1
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-04-27 01:43:42 -0600

    refactor: add logger.go to define logging middleware

--- server/server.go
+++ server/server.go
@@ -290,23 +290,3 @@ func GetCheckpointsHandler(index bleve.Index, logger *log.Logger) operations.Get
 		return operations.NewGetCheckpointsOK().WithPayload(names)
 	}
 }
-
-type responseWriter struct {
-	http.ResponseWriter
-	Code int
-}
-
-func (w *responseWriter) WriteHeader(code int) {
-	w.Code = code
-	w.ResponseWriter.WriteHeader(code)
-}
-
-func withLogger(h http.Handler, logger *log.Logger) http.HandlerFunc {
-	return func(res http.ResponseWriter, req *http.Request) {
-		w := &responseWriter{
-			ResponseWriter: res,
-		}
-		h.ServeHTTP(w, req)
-		logger.Printf("%v %v %v", w.Code, req.Method, req.URL)
-	}
-}
